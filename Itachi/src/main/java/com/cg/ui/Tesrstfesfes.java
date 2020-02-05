28e900ad5a84ea8e1755e09eb287ca74834d9961
shivamsingh7455 <shivamsingh7455@gmail.com>
2020-02-05 16:33:31 +0530

    asdsd

--- Itachi/src/main/java/com/cg/ui/Tesrstfesfes.java
+++ Itachi/src/main/java/com/cg/ui/Tesrstfesfes.java
@@ -4,6 +4,6 @@ public class Tesrstfesfes {
 public static void main(String[] args) {
 	System.out.println("asdfghjkll");
 	System.out.println("Shivam");
-	System.out.println("KAmre pe aawo");
+	System.out.println("KAmre pe aawoo");
 }
 }
